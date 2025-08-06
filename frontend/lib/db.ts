9b18a8a126d4cc402a7be1e3818033795e279ce7
matej2314 <mateo2314@gmail.com>
2025-08-06 14:54:39 +0200

    Create prisma instance and base files of the core project.

---
+++ frontend/lib/db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+import { APP_CONFIG } from '@/config/app.config';
+
+declare global {
+	// eslint-disable-next-line no-var
+	var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma || new PrismaClient();
+
+if (APP_CONFIG.nodeEnv !== 'production') {
+	global.prisma = prisma;
+}
+
+export default prisma;
